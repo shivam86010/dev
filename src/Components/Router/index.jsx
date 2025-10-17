b830f486c21cb925a218935490d411bf0c72e134
shivam86010 <94790147+shivam86010@users.noreply.github.com>
2025-10-17 23:21:12 +0530

    some error on project i trie to resolve but not resolve

--- src/Components/Router/index.jsx
+++ src/Components/Router/index.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Route , Routes } from 'react-router-dom'
 import TerminalHomePage from '../../Pages/TerminalHome/index'
 import InteractiveStoryMode from '../../Pages/InteractiveStoryMode/index'
+import ProjectsLabShowcase from '../../Pages/Projects-lab-showcase'
 function index() {
   return (
       <Routes>
         <Route path='/' element={<TerminalHomePage />} />
         <Route path='/terminalhomepage' element={<TerminalHomePage />} />
         <Route  path='/interactivestorymode' element={<InteractiveStoryMode />} />
+        <Route path='/projects-lab-showcase' element={<ProjectsLabShowcase/>}/>
+
       </Routes>
   )
 }
